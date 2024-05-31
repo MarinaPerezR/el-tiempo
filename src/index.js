e1b356c8cee17cec8a0a258451d9bd908c9b01ee
MarinaPerezR <marina.perezrobles@gmail.com>
2024-05-31 20:13:43 +0100

    Injected weather icon

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
