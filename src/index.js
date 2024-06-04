8bb6f821c4400cdb6d74773aa443d8cbb41adfba
MarinaPerezR <marina.perezrobles@gmail.com>
2024-06-04 18:14:17 +0100

    Injected the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `  
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon"><img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" 
+        alt=""
+        width="42"/></div>
+        <div class="weather-forecast-temperatures">
+           <div class="weather-forescast-temperature-max">
+              <strong>18°</strong>
+           </div>
+           <div class="weather-forecast-temperature-min">12°</div>
+        </div>
+      </div>
+    `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Zaragoza");
+displayForecast();
